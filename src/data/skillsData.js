87dd73df110d317af60dfcace812b9ddad3afce7
Gaëlle Morin <morin.gaelle@gmail.com>
2023-05-28 22:49:59 +0200

    feature: added skills component

---
+++ src/data/skillsData.js
@@ -0,0 +1,22 @@
+const skillsData = [
+    {
+        name:"HTML5"
+    },
+    {
+        name:"CSS3"
+    },
+    {
+        name:"JavaScript"
+    },
+    {
+        name:"Sass"
+    },
+    {
+        name:"Tailwind"
+    },
+    {
+        name:"Figma"
+    }
+]
+
+export default skillsData;
