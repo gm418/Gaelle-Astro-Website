be0ab573bedf5304bb813dcaa45fd42b8fa909ea
Gaëlle Morin <morin.gaelle@gmail.com>
2023-05-29 13:50:50 +0200

    feature: added qualification data

---
+++ src/data/qualificationData.js
@@ -0,0 +1,54 @@
+const qualificationData = [
+    {
+        education: [
+            {
+                studyPeriod: "Sept 2003 - juin 2006",
+                title: "Bachelière Technicienne de laboratoire",
+                school: "Institut d'Enseignement Supérieur Alexandre-André (Belgique)",
+                description: ""
+            },
+            {
+                studyPeriod: "Sept 2016 - juin 2017",
+                title: "Développeuse web",
+                school: "Wild Code School",
+                description: "Réalisation de deux projets-clients dans le cadre de ma formation à la Wild Code School. L'un en 6 semaines l'autre en 8 semaines. L'occasion d'appliquer mes connaissances pour le compte d'acteurs locaux."
+            }
+        ]
+    },
+    {
+        workExp: [
+            {
+                employmentPeriod: "Sept 2006 - août 2016",
+                jobTitle: "Technicienne de laboratoire",
+                company: "",
+                description: "J'étais principalement chargée de l'entretien, la calibration, les contrôles et la gestion des pannes sur différents automates d'analyse. Des analyses médicales des échantillons et de la validation technique des résultats. J'ai également préparé des chimiothérapies, tenu à jour des dossiers aux essais cliniques ou encore testé la qualité microbiologique de l'eau et de médicaments."
+            },
+            {
+                employmentPeriod: "Fév 2017 - juil 2017",
+                jobTitle: "Stagiaire",
+                company: "Filver",
+                description: "Stage de 6 mois dans une agence web, j'ai participé au développement d'une application en interne. J'ai surtout mis en pratique mes connaissances en frontend. J'ai aussi animé des ateliers d'initiation au code pour les enfants de 7 à 11 ans."
+            },
+            {
+                employmentPeriod: "Fév 2018 - juin 2018",
+                jobTitle: "Développeuse Rails",
+                company: "Hexagonal Consulting et jrnl",
+                description: "Chez Hexagonal Consulting, développement d'une application avec une interface admnistrateur et permettant de s'inscrire à des évènements, de noter les participants et de récupérer les notes dans un tableau à double entrées. Pour l'application jrnal, participation à la création du tableau de bord des administrateurs, à l'invitation d'utilisateurs et à la gestion des statuts d'un article."
+            },
+            {
+                employmentPeriod: "Sep 2018 - oct 2018",
+                jobTitle: "Intégratrice WordPress",
+                company: "Happy Dev",
+                description: "Accompagnée par un développeur senior, j'ai été chargée d'intégrer les pages d'un WordPress pour une agence de motion design."
+            },
+            {
+                employmentPeriod: "Déc 2018 - aujourd'hui",
+                jobTitle: "Intégratrice",
+                company: "Startin'Blox",
+                description: "Développement d'une application pour faciliter le travail entre freelances, associations, etc. J'ai intégré les maquettes fournies par la graphiste et affiché des données dynamiques. La structure HTML est générée par Startin'blox et j'utilise les composants développés par l'équipe de développeurs pour les adapter graphiquement."
+            }
+        ]
+    }
+];
+
+export default qualificationData
