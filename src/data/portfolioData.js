cd69440d9b0f8448472211f3cc667683db3e07bd
Gaëlle Morin <morin.gaelle@gmail.com>
2023-05-30 19:28:09 +0200

    feature: added portfolio's data

---
+++ src/data/portfolioData.js
@@ -0,0 +1,24 @@
+const portfolioData = {
+    project1: {
+        title: "MAM aux Trésors",
+        techs: ["MongoDB", "Express", "AngularJS", "NodeJS", "JavaScript", "Materialize"],
+        content: "Création d'un site vitrine et partiellement administrable pour une Maison d'Assistantes Maternelles appelée la MAM Aux Trésors. Elle souhaitaient mettre en évidence l'équipe et présenter brièvement leur établissement et les activités proposées. Elles peuvent éditer les parties 'Partenaires' et ... Grâce à ce projet, nous avons mis en application nos connaissances fraîchement acquises sur le framework Materialize."
+    },
+    project2: {
+        title: "BIJ et BIJ Web",
+        techs: ["Electron", "MongoDB", "Express", "AngularJS", "NodeJS", "JavaScript", "Materialize"],
+        content: ""
+    },
+    project3: {
+        title: "Rate Me",
+        techs: [],
+        content: ""
+    },
+    project4: {
+        title: "Orbit",
+        techs: [],
+        content: ""
+    }
+};
+
+export default portfolioData;
