b3883053706b7046944d928e10e640bf7a6a6ea0
Gaëlle Morin <morin.gaelle@gmail.com>
2023-05-30 19:50:28 +0200

    feature: completeded portfolio's data

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -7,16 +7,16 @@ const portfolioData = {
     project2: {
         title: "BIJ et BIJ Web",
         techs: ["Electron", "MongoDB", "Express", "AngularJS", "NodeJS", "JavaScript", "Materialize"],
-        content: ""
+        content: "Sous la direction de l'Union Nationale de l'Information Jeunesse (UNIJ), le Bureau de l'Information Jeunesse (BIJ) souhaitait une application bureau et web permettant à n'importe quel forma.teur.trice de leur Bureau d'accéder, online et offline, à ses cours. L'intérêt était de transposer leur support de formation dans une application afin qu'elle soit téléchargeable et pouvoir l'utiliser même là où il n'y a pas Internet afin d'éviter le transport de documents physiques pouvant être lourds. Il fallait également une partie admin afin de pouvoir créer/supprimer des comptes utilisateurs."
     },
     project3: {
         title: "Rate Me",
-        techs: [],
-        content: ""
+        techs: ["Ruby on Rails", "JavaScript", "Bootstrap", "AWS-sdk", "Google API"],
+        content: "Cette application permet aux utilisateurs, via un mail envoyé par l'administrateur, de se connecter via leur compte Google. Ils peuvent ensuite créer un évènement et voir les évènements passés et ceux auxquels ils peuvent s' inscrire grâce à un code couleur. Après la fin de l'évènement, les participants peuvent se noter entre eux et ajouter un commentaire. Chaque note est symbolisée par une couleur."
     },
     project4: {
         title: "Orbit",
-        techs: [],
+        techs: ["HTML5", "CSS3", "Scss", "JavaScript", "Startin'blox"],
         content: ""
     }
 };
