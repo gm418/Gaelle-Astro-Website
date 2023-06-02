aec011eafacbd505336ea73e528511d520208632
Gaëlle Morin <morin.gaelle@gmail.com>
2023-06-02 23:19:14 +0200

    feature: added images to portfolio component

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -2,22 +2,26 @@ const portfolioData = {
     project1: {
         title: "MAM aux Trésors",
         techs: ["MongoDB", "Express", "AngularJS", "NodeJS", "JavaScript", "Materialize"],
-        content: "Création d'un site vitrine et partiellement administrable pour une Maison d'Assistantes Maternelles appelée la MAM Aux Trésors. Elle souhaitaient mettre en évidence l'équipe et présenter brièvement leur établissement et les activités proposées. Elles peuvent éditer les parties 'Partenaires' et ... Grâce à ce projet, nous avons mis en application nos connaissances fraîchement acquises sur le framework Materialize."
+        content: "Création d'un site vitrine et partiellement administrable pour une Maison d'Assistantes Maternelles appelée la MAM Aux Trésors. Elle souhaitaient mettre en évidence l'équipe et présenter brièvement leur établissement et les activités proposées. Elles peuvent éditer les parties 'Partenaires' et ... Grâce à ce projet, nous avons mis en application nos connaissances fraîchement acquises sur le framework Materialize.",
+        images: ["/images/mam-lg.jpg", "/images/mam-tb.jpg", "/images/mam-mb.jpg"]
     },
     project2: {
         title: "BIJ et BIJ Web",
         techs: ["Electron", "MongoDB", "Express", "AngularJS", "NodeJS", "JavaScript", "Materialize"],
-        content: "Sous la direction de l'Union Nationale de l'Information Jeunesse (UNIJ), le Bureau de l'Information Jeunesse (BIJ) souhaitait une application bureau et web permettant à n'importe quel forma.teur.trice de leur Bureau d'accéder, online et offline, à ses cours. L'intérêt était de transposer leur support de formation dans une application afin qu'elle soit téléchargeable et pouvoir l'utiliser même là où il n'y a pas Internet afin d'éviter le transport de documents physiques pouvant être lourds. Il fallait également une partie admin afin de pouvoir créer/supprimer des comptes utilisateurs."
+        content: "Sous la direction de l'Union Nationale de l'Information Jeunesse (UNIJ), le Bureau de l'Information Jeunesse (BIJ) souhaitait une application bureau et web permettant à n'importe quel forma.teur.trice de leur Bureau d'accéder, online et offline, à ses cours. L'intérêt était de transposer leur support de formation dans une application afin qu'elle soit téléchargeable et pouvoir l'utiliser même là où il n'y a pas Internet afin d'éviter le transport de documents physiques pouvant être lourds. Il fallait également une partie admin afin de pouvoir créer/supprimer des comptes utilisateurs.",
+        images: ["/images/bij-lg.jpg", "/images/mam-tb.jpg"]
     },
     project3: {
         title: "Rate Me",
         techs: ["Ruby on Rails", "JavaScript", "Bootstrap", "AWS-sdk", "Google API"],
-        content: "Cette application permet aux utilisateurs, via un mail envoyé par l'administrateur, de se connecter via leur compte Google. Ils peuvent ensuite créer un évènement et voir les évènements passés et ceux auxquels ils peuvent s' inscrire grâce à un code couleur. Après la fin de l'évènement, les participants peuvent se noter entre eux et ajouter un commentaire. Chaque note est symbolisée par une couleur."
+        content: "Cette application permet aux utilisateurs, via un mail envoyé par l'administrateur, de se connecter via leur compte Google. Ils peuvent ensuite créer un évènement et voir les évènements passés et ceux auxquels ils peuvent s' inscrire grâce à un code couleur. Après la fin de l'évènement, les participants peuvent se noter entre eux et ajouter un commentaire. Chaque note est symbolisée par une couleur.",
+        images: ["/images/hexa-lg.jpg", "/images/hexa-tb.jpg"]
     },
     project4: {
         title: "Orbit",
         techs: ["HTML5", "CSS3", "Scss", "JavaScript", "Startin'blox"],
-        content: ""
+        content: "",
+        images: ["/images/orbit-lg.jpg", "/images/orbit-tb.jpg", "/images/orbit-mb.jpg"]
     }
 };
 
