0b82057ceee73d1570c63ebf359c3131f4f903a5
Gaëlle Morin <morin.gaelle@gmail.com>
2023-06-04 10:55:00 +0200

    feature: added footer data

---
+++ src/data/footerData.js
@@ -0,0 +1,50 @@
+const footerData = {
+    navLinks: [
+        {
+            href: "#skills",
+            title: "Compétences"
+        },
+        {
+            href: "#portfolio",
+            title: "Portfolio"
+        },
+        {
+            href: "#contact",
+            title: "Contact"
+        }
+    ],
+    socialNetworks: [
+        {
+            href: "https://www.linkedin.com/in/gaelle-morin/",
+            icon: "linkedin-alt"
+        },
+        {
+            href: "",
+            icon: "gitlab"
+        },
+        {
+            href: "https://github.com/daedalus418",
+            icon: "github-alt"
+        }
+    ],
+    iconsSources: [
+        {
+            href: "https://iconscout.com/contributors/unicons",
+            title: "Unicons Font"
+        },
+        {
+            href: "https://iconscout.com/contributors/jagathish",
+            title: "Jagathish"
+        },
+        {
+            href: "https://iconscout.com/contributors/icon-54",
+            title: "Icon 54"
+        },
+        {
+            href: "https://iconscout.com",
+            title: "Iconscout"
+        },
+    ]
+};
+
+export default footerData;
