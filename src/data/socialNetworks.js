18bdf7e0fb7c8385a53dd39090083e24583026cb
Gaëlle Morin <morin.gaelle@gmail.com>
2023-06-06 12:58:49 +0200

    feature: added socialNetworks data

---
+++ src/data/socialNetworks.js
@@ -0,0 +1,16 @@
+const socialNetworksData = [
+    {
+        href: "https://www.linkedin.com/in/gaelle-morin/",
+        icon: "linkedin-alt"
+    },
+    {
+        href: "",
+        icon: "gitlab"
+    },
+    {
+        href: "https://github.com/daedalus418",
+        icon: "github-alt"
+    }
+];
+
+export default socialNetworksData;
