dddcd953b08078f2410398755dc6ceb592103e1b
Gaëlle Morin <morin.gaelle@gmail.com>
2023-07-30 15:16:19 +0200

    feature: added alternative text to social links in footer

--- src/data/footerData.js
+++ src/data/footerData.js
@@ -16,15 +16,18 @@ const footerData = {
     socialNetworks: [
         {
             href: "https://www.linkedin.com/in/gaelle-morin/",
-            icon: "linkedin-alt"
+            icon: "linkedin-alt",
+            textAlternative:"Lien vers mon profil LinkedIn"
         },
         {
             href: "",
-            icon: "gitlab"
+            icon: "gitlab",
+            textAlternative:"Lien vers mon profil Gitlab"
         },
         {
             href: "https://github.com/daedalus418",
-            icon: "github-alt"
+            icon: "github-alt",
+            textAlternative:"Lien vers mon profil GitHub"
         }
     ],
     iconsSources: [
