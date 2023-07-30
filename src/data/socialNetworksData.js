1b924ea4ca80d38f5bccd6d3fe82e4cd1e67b8f6
Gaëlle Morin <morin.gaelle@gmail.com>
2023-07-30 16:02:17 +0200

    update: added the possibility to set aria-hidden into links

--- src/data/socialNetworksData.js
+++ src/data/socialNetworksData.js
@@ -1,15 +1,18 @@
 const socialNetworksData = [
     {
         href: "https://www.linkedin.com/in/gaelle-morin/",
-        icon: "linkedin-alt"
+        icon: "linkedin-alt",
+        textAlternative:"Lien vers mon profil LinkedIn"
     },
     {
         href: "",
-        icon: "gitlab"
+        icon: "gitlab",
+        textAlternative:"Lien vers mon profil Gitlab"
     },
     {
         href: "https://github.com/daedalus418",
-        icon: "github-alt"
+        icon: "github-alt",
+        textAlternative:"Lien vers mon profil GitHub"
     }
 ];
 
