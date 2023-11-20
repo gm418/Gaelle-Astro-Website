413b476f7b1076f453ea92db6fb940af90b2ad5f
Gaëlle Morin <morin.gaelle@gmail.com>
2023-11-20 17:04:30 +0100

    portfolio: added accessibility - added HTML attributes for assistive technologies and better HTML mark up

--- src/scripts/portfolio-show-hide.js
+++ src/scripts/portfolio-show-hide.js
@@ -4,7 +4,11 @@ const portfolioInformation = document.querySelector('.portfolio__information'),
 portfolioContents.forEach(portfolioContent => {
     portfolioContent.addEventListener('click', () => {
 
-        portfolioContents.forEach(elem => elem.classList.remove('js-portfolio__active'));
+        portfolioContents.forEach(elem => {
+            elem.classList.remove('js-portfolio__active');
+            elem.setAttribute('aria-expanded', 'false');
+        });
         portfolioContent.classList.add('js-portfolio__active');
+        portfolioContent.setAttribute('aria-expanded', 'true');
     });
 });
